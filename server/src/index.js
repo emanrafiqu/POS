f96b007653893bdd0ed045be6de232cfa3094eb4
Eman Rafique <emanrafique50301@gmail.com>
2026-07-07 23:38:19 +0500

    Fix Railway host binding

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,6 @@ import app from './app.js';
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`[Veloura] API server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`[Veloura] API server running on port ${PORT}`);
 });
